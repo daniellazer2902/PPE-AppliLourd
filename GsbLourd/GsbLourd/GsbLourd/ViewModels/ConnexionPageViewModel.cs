6ec13c8399d793af05c0ec09a6012997ae010d93
hugo95470 <cabaret.hugo@orange.fr>
2021-04-02 09:36:13 +0200

    page midec faite

--- GsbLourd/GsbLourd/GsbLourd/ViewModels/ConnexionPageViewModel.cs
+++ GsbLourd/GsbLourd/GsbLourd/ViewModels/ConnexionPageViewModel.cs
@@ -22,12 +22,6 @@ namespace GsbLourd.ViewModels
             Title = "Connexion Page";
         }
 
-        public User Utilisateur
-        {
-            get { return _utilisateur; }
-            set { SetProperty(ref _utilisateur, value); }
-        }
-        private User _utilisateur;
         public String Identifiant
         {
             get { return _identifiant; }
@@ -41,12 +35,6 @@ namespace GsbLourd.ViewModels
             set { SetProperty(ref _motDePasse, value); }
         }
         private String _motDePasse;
-        public JObject Answer
-        {
-            get { return _answer; }
-            set { SetProperty(ref _answer, value); }
-        }
-        private JObject _answer;
 
         public Boolean Erreur
         {
@@ -73,7 +61,7 @@ namespace GsbLourd.ViewModels
                 {
                     var answer = await response.Content.ReadAsStringAsync();
 
-                    Answer = JObject.Parse(answer);
+                    var Answer = JObject.Parse(answer);
                     if ((string)Answer["VIS_NOM"] == Identifiant)
                     {
                         NavigationParameters navigationParameters = new NavigationParameters();
@@ -97,19 +85,6 @@ namespace GsbLourd.ViewModels
         public virtual bool CanExecuteConnexionCommand()
         {
             return true;
-        }
-
-        public class User
-        {
-            public string VIS_MATRICULE { get; set; }
-            public string VIS_NOM { get; set; }
-            public string Vis_PRENOM { get; set; }
-            public string VIS_ADRESSE { get; set; }
-            public string VIS_VILLE { get; set; }
-            public string SEC_CODE { get; set; }
-            public string LAB_CODE { get; set; }
-            public int VIS_CP { get; set; }
-            public DateTime VIS_DATEEMBAUCHE { get; set; }
         }
     }
 }
--- GsbLourd/GsbLourd/GsbLourd/ViewModels/MedicamentsPageViewModel.cs
+++ GsbLourd/GsbLourd/GsbLourd/ViewModels/MedicamentsPageViewModel.cs
@@ -1,17 +1,145 @@
-﻿using Prism.Navigation;
+﻿using Newtonsoft.Json.Linq;
+using Prism.Commands;
+using Prism.Navigation;
 using System;
+using System.Net.Http;
 
 namespace GsbLourd.ViewModels
 {
     public class MedicamentsPageViewModel : ViewModelBase
     {
+        private readonly HttpClient _client;
         private readonly INavigationService _navigationService;
         public MedicamentsPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
+            _client = new HttpClient();
             _navigationService = navigationService;
             Title = "Rapport de visite Page";
         }
 
+        public async override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            Numero = 1;
+            RecupMedicament();
+
+            base.OnNavigatedTo(parameters);
+        }
+
+        public int Numero
+        {
+            get { return _numero; }
+            set { SetProperty(ref _numero, value); }
+        }
+        private int _numero;
+
+        public string Code
+        {
+            get { return _code; }
+            set { SetProperty(ref _code, value); }
+        }
+        private string _code;
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { SetProperty(ref _nom, value); }
+        }
+        private string _nom;
+
+        public string Famille
+        {
+            get { return _famille; }
+            set { SetProperty(ref _famille, value); }
+        }
+        private string _famille;
+
+        public string Prix
+        {
+            get { return _prix; }
+            set { SetProperty(ref _prix, value); }
+        }
+        private string _prix;
+
+        public string ContreIndication
+        {
+            get { return _contreIndication; }
+            set { SetProperty(ref _contreIndication, value); }
+        }
+        private string _contreIndication;
+
+        public string Effet
+        {
+            get { return _effet; }
+            set { SetProperty(ref _effet, value); }
+        }
+        private string _effet;
+
+        public string Composition
+        {
+            get { return _composition; }
+            set { SetProperty(ref _composition, value); }
+        }
+        private string _composition;
+
+
+
+        public async void RecupMedicament()
+        {
+            Uri uri = new Uri("https://hugocabaret.onthewifi.com/GSB/APIGSB/requetes/GetMedicaments.php?MED_NUM=" + Numero.ToString());
+            Console.WriteLine("{0} URI", uri);
+
+            HttpResponseMessage response = await _client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var answer = await response.Content.ReadAsStringAsync();
+
+                var Answer = JObject.Parse(answer);
+
+                Code = (string)Answer["MED_DEPOTLEGAL"];
+                Nom = (string)Answer["MED_NOMCOMMERCIAL"];
+                Famille = (string)Answer["FAM_LIBELLE"];
+                Prix = (string)Answer["MED_PRIXECHANTILLON"];
+                ContreIndication = (string)Answer["MED_CONTREINDIC"];
+                Effet = (string)Answer["MED_EFFETS"];
+                Composition = (string)Answer["MED_COMPOSITION"];
+            }
+        }
+
+        //SUIVANT
+        public DelegateCommand Suivant
+        {
+            get { return _suivant ?? (_suivant = new DelegateCommand(ExecuteSuivant, CanExecuteSuivant)); }
+        }
+        private DelegateCommand _suivant;
+
+        public async void ExecuteSuivant()
+        {
+            Numero++;
+            RecupMedicament();
+        }
+
+        public virtual bool CanExecuteSuivant()
+        {
+            return true;
+        }
+
+        //PRECENDET
+        public DelegateCommand Precedent
+        {
+            get { return _precedent ?? (_precedent = new DelegateCommand(ExecutePrecedent, CanExecutePrecedent)); }
+        }
+        private DelegateCommand _precedent;
+
+        public async void ExecutePrecedent()
+        {
+            Numero++;
+            RecupMedicament();
+        }
+
+        public virtual bool CanExecutePrecedent()
+        {
+            return true;
+        }
     }
 }
